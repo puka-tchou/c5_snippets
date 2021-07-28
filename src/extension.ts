b06fcc93f831eecefdca40e515de4bcd4e86566e
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-07-28 20:08:32 +0200

    Ajout des menu et des commande de création des controleurs de page_type et de singlepage

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,21 @@
 import * as vscode from 'vscode';
-
-let myStatusBarItem: vscode.StatusBarItem;
+import {generateSinglePage} from "./controllers/SinglePageController";
+import {generatePageType} from "./controllers/PageTypeController";
 
 export function activate({subscriptions}: vscode.ExtensionContext) {
 	
-	const myCommandId = 'c5.showHelpers';
+	const myCommandId = 'c5helper.showHelpers';
 	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
 		vscode.window.showInformationMessage(`Afficher les helper concrete`);
 	}));
 
+	subscriptions.push(vscode.commands.registerCommand('c5helper.createSinglePageController', async ({path}) => {
+		generateSinglePage(path);
+	}));
 
-
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.command = myCommandId;
-	myStatusBarItem.text = 'C5';
-	myStatusBarItem.show();
-	subscriptions.push(myStatusBarItem);
+	subscriptions.push(vscode.commands.registerCommand('c5helper.createPageTypeController', async ({path}) => {
+		generatePageType(path);
+	}));
 }
 
 // this method is called when your extension is deactivated

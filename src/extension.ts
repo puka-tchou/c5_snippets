678e0c4acd56dfd091a460979a6a227c6c41ad0a
puka <gaspard@6tematik.fr>
2021-07-28 17:36:28 +0200

    style: prettier

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,22 @@ import * as vscode from 'vscode';
 
 let myStatusBarItem: vscode.StatusBarItem;
 
-export function activate({subscriptions}: vscode.ExtensionContext) {
-	
-	const myCommandId = 'c5.showHelpers';
-	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
-		vscode.window.showInformationMessage(`Afficher les helper concrete`);
-	}));
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+  const myCommandId = 'c5.showHelpers';
+  subscriptions.push(
+    vscode.commands.registerCommand(myCommandId, () => {
+      vscode.window.showInformationMessage(`Afficher les helper concrete`);
+    })
+  );
 
-
-
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.command = myCommandId;
-	myStatusBarItem.text = 'C5';
-	myStatusBarItem.show();
-	subscriptions.push(myStatusBarItem);
+  myStatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  myStatusBarItem.command = myCommandId;
+  myStatusBarItem.text = 'C5';
+  myStatusBarItem.show();
+  subscriptions.push(myStatusBarItem);
 }
 
 // this method is called when your extension is deactivated

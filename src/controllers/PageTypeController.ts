402f388c762b05f9bc36b631091cbfa91b7e01bf
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-07-29 11:03:30 +0200

    Modication des helper

--- src/controllers/PageTypeController.ts
+++ src/controllers/PageTypeController.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
 import * as Path from 'path';
 
 export async function generatePageType(path: string) {
@@ -12,7 +11,7 @@ export async function generatePageType(path: string) {
     .join('');
 
   var realname = singlePageController + '.php';
-  const filePath = Path.join(path, realname);
+  const filePath = Path.join(path, realname).substring(1);
 
   const content = [
     `<?php`,
@@ -29,18 +28,18 @@ export async function generatePageType(path: string) {
     `}`,
   ].join('\n');
 
-  fs.appendFile(filePath.substring(1), content, (err) => {
-    if (err) {
-      vscode.window.showErrorMessage(err.message);
-    } else {
-      vscode.workspace.openTextDocument(filePath).then(
-        (a: vscode.TextDocument) => {
-          vscode.window.showTextDocument(a);
-        },
-        (error: any) => {
-          vscode.window.showErrorMessage(error);
-        }
-      );
+  const wsEdit = new vscode.WorkspaceEdit();
+  const uri = vscode.Uri.file(filePath);
+  wsEdit.createFile(uri, { ignoreIfExists: true });
+  wsEdit.insert(uri, new vscode.Position(0, 0), content);
+  vscode.workspace.applyEdit(wsEdit);
+  vscode.workspace.openTextDocument(filePath).then(
+    (a: vscode.TextDocument) => {
+      a.save();
+      vscode.window.showTextDocument(a);
+    },
+    (error: any) => {
+      vscode.window.showErrorMessage(error);
     }
-  });
+  );
 }

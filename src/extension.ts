78769bc2cc7753c12b385bb953aff8fc8bd50802
puka <gaspard@6tematik.fr>
2021-12-16 13:32:06 +0100

    fix(release): corrige la compilation

---
+++ src/extension.ts
@@ -0,0 +1,6 @@
+import * as vscode from 'vscode';
+
+export function activate({}: vscode.ExtensionContext) {}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
